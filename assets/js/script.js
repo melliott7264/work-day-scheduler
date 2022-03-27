a068af6832122d43dcbd28f7b16c0b7930e4606e
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-03-27 13:16:35 -0400

    got datepicker working acceptably

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,19 @@ var endTime = 17;  // end of work day 24 hour format
 // Define events array for localStorage  consists of event objects with properties dateTime and eventDesc  
 var eventsArray = [];
 // Need to define currentDate for use across functions and event handlers
-var currentDate = "";
+var currentDate = moment().format("MM/DD/YYYY");
+
 
 // Add current day to top of page 
 // $("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
 
 /* ******** Start listEvents function ********* */
 // list out all the schedulable blocks in the current day 
-var listEvents = function (eventDesc, eventId) {
+// var listEvents = function (eventDesc, eventId) {
+var listEvents = function () {
+
+ displayDate = moment(currentDate).format("dddd, MMMM Do YYYY");
+ $("#displayDate").text(displayDate);
 
 //eventDesc and eventId are passed by the event handlers when editing events and by the saveEvent function.  Otherwise, they are ignored.
 
@@ -71,46 +76,44 @@ for ( i=startTime; i<=endTime; i++) {
     }
 
   // if an eventId and eventDesc have been passed to this function used those values.  Otherwise check for saved events.  If none, create an empty event block
-  if (eventId && eventDesc) {
-    if (eventId === dateIdHour){
-        eventDescription = eventDesc;
-    } else {
-        // console.log("passed eventId and dataIdHour do not match ");
-    }
-} else {
+//   if (eventId && eventDesc) {
+//     if (eventId === dateIdHour){
+//         eventDescription = eventDesc;
+//     } else {
+//         console.log("a matching eventID was not passed, event description will be blank");
+//     }
+//   } else {
        // check if saved events are present in localStorage.  if so, check for a matching eventId and set eventDescription = eventArray.eventDesc  
         if (loadEvents()) { 
         // loop through the entire array looking for a matching eventId
             for ( j = 0; j < eventsArray.length; j++){
                 if (eventsArray[j].eventId === dateIdHour) {
                     eventDescription = eventsArray[j].eventDesc;
-                    // console.log(dateIdHour + " " + eventsArray[j].eventId + " " + eventsArray[j].eventDesc);
+                    console.log(eventDescription);
                 } else {
-                    // console.log("there is not an event for this date/time in the database");
-                   
+                    console.log("there is not an event for this date/time in the database, event description will be blank");
                 }
             }   
         } else {
-            console.log("events file failed to load");
+            console.log("events file failed to load, event description will be blank");
            
         }
-}
+    // }
+
+    // Creating the elements for each event hour block
 
     // creating a <li> for each event hour
     var listItemEl = $("<li>").addClass("event-list-item list-unstyled row").attr("id", dateIdHour );
     listGroupEl.append(listItemEl);
-    
+
     // creating <span>, <p>, and <button> elements for each event hour and appending to the <li>
     var listItemSpanEl = $("<span>").addClass("col-2 d-flex align-items-center justify-content-center time-block hour " + hourStatus(dateIdHourM)).text(displayHourString + ":00 " + amString);
     var listItemPEl = $("<p>").addClass("col-9 d-flex align-items-center mb-0 description " + hourStatus(dateIdHourM)).text(eventDescription);
     var listItemBtnEl = $("<button>").addClass("saveBtn col-1 " + hourStatus(dateIdHourM)).text("Save");
     listItemEl.append(listItemSpanEl, listItemPEl, listItemBtnEl);
 }
-
 return;
-
 }; 
-
 /* ************ End listEvent function ************ */
 
 /* *********** Start loadEvents function to load events from localStorage ********** */
@@ -118,13 +121,12 @@ var loadEvents = function() {
 
     if (localStorage.getItem("events")) {
         eventsArray = JSON.parse(localStorage.getItem("events"));
-        // console.log(eventsArray);
+        console.log("loaded local database file");
         return true;
     } else {
         return false;
     }
 };
-
 /* **************  End loadEvent function ************** */
 
 /* ************ Start saveEvents function to save events array to localStorage events file ************ */
@@ -142,7 +144,9 @@ var saveEvents = function(eventDesc, eventId) {
 
     return true;
 }
+/* ************* End saveEvents function ************ */
 
+/* ************ Start dateFormat function ************* */
 // a function to take the string mm/dd/yyyy and turn it into the string yyyymmdd for the eventId
 var dateFormat = function (inputDateString) {
     var monthArray = [];
@@ -165,7 +169,10 @@ var dateFormat = function (inputDateString) {
     console.log(reverseDateString);
     return reverseDateString;
 };
+/* ************ End dateFormat function ************** */
 
+/* ******** Start hourStatus function ********** */
+// this function determines whether the current hour block is in the past, present, or future
 var hourStatus = function (dateIdHourM) {
        //get the current date and time and compare to the passed dateIdHour (yyyymmddhh) and assign a class: past, present, future
    if (moment().isBefore(dateIdHourM, "hour")){
@@ -178,47 +185,81 @@ var hourStatus = function (dateIdHourM) {
 
        return hourClass;
 }
+/* ************* End hourStatus function *********** */
 
+/* ********** Start updateScreen function *********** */
+// updates the screen every minute to catch when the hour changes
+var updateScreen = function () {
+    var intervalId = setInterval(listEvents(), 60000);
+}
+/* ********** End updateScreen function ************ */
+
+/* ************ Event Handler for Editing the Event Description ************* */
 // need to identify the element that was clicked on and replace it with an input form  
-$(".container").on("click", "p", function(){
-    var eventDesc =$(this).text().trim();
-    // var eventId = $(this).closest(".event-list-item").attr("id");
-    var eventDescInput = $("<textarea>").addClass("form-control").val(eventDesc);
-    $(this).replaceWith(eventDescInput);
-    // make the text to be edited in focus
-    eventDescInput.trigger("focus");
-    listEvents(eventDesc, eventId);
-    // listEvents();
-  });
-  
-  $(".container").on("blur", "textarea", function() {
-    //get the edit box's current value/text
-    var eventDesc = $(this).val().trim();
-    var eventId = $(this).closest(".event-list-item").attr("id");
-    $(this).replaceWith=$("<p>").addClass("col-9 d-flex align-items-center mb-0 description " + hourStatus()).text(eventDesc);
-    // pass the updated event description along with the eventId back to listEvents to display
-    saveEvents(eventDesc, eventId);
-    listEvents(eventDesc, eventId);
-    // listEvents();
-  });
-  
-$(".container").on("click", "button", function () {
-    var eventId=$(this).closest(".event-list-item").attr("id");
-    var eventDesc = $(this).siblings("p").text().trim();
-    saveEvents(eventDesc, eventId);
+// $(function() {
+    $(".container").on("click", "p", function(){
+        var eventDesc1 =$(this).text().trim();
+        var eventDescInput = $("<textarea>").addClass("form-control").val(eventDesc1);
+        $(this).replaceWith(eventDescInput);
+        // make the text to be edited in focus
+        eventDescInput.trigger("focus");
+        // var eventId1 = $(this).closest(".event-list-item").attr("id");
+        console.log("clicked on p -- " + " eventDesc1 " + eventDesc1 + "  eventId1 " + eventId1);
+        // listEvents(eventDesc1, eventId1);
+        listEvents();
     });
+// }):
 
-$("#datepicker").datepicker( {
+// $(function(){
+    // saves the editing changes when you click off the edited element 
+  $(".container").on("mouseleave", "textarea", function() {
+    //get the edit box's current value/text
+    var eventDesc2 = $(this).val().trim();
+    var eventId2 = $(this).closest(".event-list-item").attr("id");
+    $(this).replaceWith=$("<p>").addClass("col-9 d-flex align-items-center mb-0 description " + hourStatus()).text(eventDesc2);
+    // pass the updated event description along with the eventId back to listEvents to display
+    console.log("mouseleave p -- " + eventDesc2 + "  " + eventId2);
+    saveEvents(eventDesc2, eventId2);
+    // listEvents(eventDesc2, eventId2);
+    listEvents();
+    // listEvents();
+  }); 
+// }); 
+/* *********** End Event Description Editing  ************* */
+
+/* ********** Start Event Handler for Event Save Button *********** */   
+// this is a redundant function to meet acceptance criteria.  Edits are saved when you click off the edited element 
+// $(function(){
+   $(".container").on("click", "button", function () {
+    var eventId3=$(this).closest(".event-list-item").attr("id");
+    var eventDesc3 = $(this).siblings("p").text().trim();
+    saveEvents(eventDesc3, eventId3);
+    }); 
+// });
+
+/* ************* Start datepicker Event Handler ************** */
+// $(function()  {
+//     $("#datepicker").datepicker().datepicker("setDate", new Date ());
+//     currentDate = $("#datepicker").datepicker("getDate");
+//     listEvents();
+// });    
+// $(function(){
+    $("#datepicker").datepicker( {
         changeMonth: true,
         changeYear: true,
         dateFormat: "mm/dd/yy",
+        defaultDate: new Date(),
         onSelect: function(date) {
             currentDate = date;
             listEvents();
         },
-});
+    });
+// });
 
+/* *************  End datepicker Event Handler ************* */
 
+// Initial load of calendar for current day
+updateScreen();
 listEvents();
 
 
